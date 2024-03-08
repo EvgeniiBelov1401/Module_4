0d95fe9b3322150b57c2442006d4c7bf4ce6d0eb
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-08 16:32:49 +0300

    Добавил Exercise_4.1.18

---
+++ Module_4/Exercise_4.1.18/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Exercise_4._1._18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            switch (Console.ReadLine())
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Your color is red!");
+                    break;
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Your color is green!");
+                    break;
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Your color is cyan");
+                    break;
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Your color is yellow!");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
