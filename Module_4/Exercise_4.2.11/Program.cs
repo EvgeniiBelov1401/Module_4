afc12b56dafabb4b2b1fc83f4e0eb6759b2b06b9
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-08 18:57:45 +0300

    Добавил Exercise_4.2.11

---
+++ Module_4/Exercise_4.2.11/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Exercise_4._2._11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = 0;
+            do
+            {
+                switch (Console.ReadLine())
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Your color is red!");
+                        break;
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Your color is green!");
+                        break;
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Your color is cyan");
+                        break;
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Your color is yellow!");
+                        break;
+                }
+                t++;
+            }while (t<3);
+        }
+    }
+}
