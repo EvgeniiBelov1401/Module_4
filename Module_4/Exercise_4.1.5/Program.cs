c4a75a5514b5377c84ea7556388b64b969dfa2a1
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-08 15:09:41 +0300

    Добавил Exercise_4.1.5

---
+++ Module_4/Exercise_4.1.5/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Exercise_4._1._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A;
+            int B;
+            double X;
+            double Y;
+            bool isAlessBorXmoreY;
+
+            Console.Write("Введите значение A: ");
+            A=int.Parse(Console.ReadLine());
+            Console.Write("Введите значение B: ");
+            B = int.Parse(Console.ReadLine());
+            Console.Write("Введите значение X: ");
+            X = double.Parse(Console.ReadLine());
+            Console.Write("Введите значение Y: ");
+            Y = double.Parse(Console.ReadLine());
+
+            isAlessBorXmoreY = A < B | X > Y;
+
+            Console.WriteLine($"A < B или X > Y - {isAlessBorXmoreY}");
+            Console.ReadKey();
+        }
+    }
+}
