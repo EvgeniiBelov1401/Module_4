c35e626e2edee053c0b556064e5ab6865582d8a6
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-08 15:44:14 +0300

    Добавил Exercise_4.1.11

---
+++ Module_4/Exercise_4.1.11/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercise_4._1._11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 6;
+            var b=7;
+
+            if (a == b) 
+            {
+                Console.WriteLine("Условие истинно");
+            }
+            else
+            {
+                Console.WriteLine("Условие ложно");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
