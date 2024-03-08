0c413537911a04268266ee9c78fba61b24019c38
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-08 15:55:44 +0300

    Добавил Exercise_4.1.15

---
+++ Module_4/Exercise_4.1.15/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Exercise_4._1._15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 6;
+            var b = 7;
+            var c = a != b ? a + b : b;
+            Console.WriteLine(c);
+            Console.ReadKey();
+        }
+    }
+}
