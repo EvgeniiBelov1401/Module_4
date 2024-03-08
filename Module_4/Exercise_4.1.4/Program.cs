666255e8074260c26d78864c9724e88b4c7f426f
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-08 14:56:22 +0300

    Добавил Exercise_4.1.4

---
+++ Module_4/Exercise_4.1.4/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercise_4._1._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string line1;
+            string line2;
+            bool isLine1eqLine2;
+
+            Console.Write("Введите строку №1: ");
+            line1 = Console.ReadLine();
+            Console.Write("Введите строку №1: ");
+            line2 = Console.ReadLine();
+
+            isLine1eqLine2 = line1==line2;
+
+            Console.WriteLine($"Строка1 и Строка2 равны? - {isLine1eqLine2}");
+            Console.ReadKey();
+        }
+    }
+}
