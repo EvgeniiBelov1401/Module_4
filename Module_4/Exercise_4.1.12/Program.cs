6a04d2d264c7846f65e9580dce503e9eee372ef5
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-08 15:46:21 +0300

    Добавил Exercise_4.1.12

---
+++ Module_4/Exercise_4.1.12/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercise_4._1._12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 6;
+            var b = 6;
+
+            if (a == b)
+            {
+                Console.WriteLine("Условие истинно");
+            }
+            else
+            {
+                Console.WriteLine("Условие ложно");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
