5889c72fb1707f7dba49cb50c9f6ca06bb7bacfd
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-09 14:13:37 +0300

    Добавил Exercise_4.3.10

---
+++ Module_4/Exercise_4.3.10/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Exercise_4._3._10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = { { 1,2,3},{5,6,7 } };
+            Console.WriteLine(array.Length);
+        }
+    }
+}
