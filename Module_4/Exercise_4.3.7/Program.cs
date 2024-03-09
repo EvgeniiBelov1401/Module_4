9119f10f0287d2fde9a640e46d727a27a4e2b952
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-09 13:59:50 +0300

    Добавил Exercise_4.3.7

---
+++ Module_4/Exercise_4.3.7/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Exercise_4._3._7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите ваше имя:");
+            string name=Console.ReadLine();
+            Console.Write("\nВаше имя наоборот:");
+            for (int i=name.Length-1;i>=0;i--)
+            {
+                Console.Write(name[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
