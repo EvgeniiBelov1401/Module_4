06335b03f7d4acf01f85f98d523e3e2e4a3cd66f
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-09 16:04:46 +0300

    Добавил Exercise_4.3.15

---
+++ Module_4/Exercise_4.3.15/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercise_4._3._15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var array= new[] {1,-1,2,-2,3,-3,4,-4,5,-5,6,-6,7,8,9,10,-10,11,0,12};
+            var CounterPositiveNum = 0;
+
+            foreach(var item in array)
+            {
+                if (item > 0)
+                {
+                    CounterPositiveNum++;
+                }
+            }
+            Console.WriteLine($"Кол-во положительных чисел = {CounterPositiveNum}");
+        }
+    }
+}
