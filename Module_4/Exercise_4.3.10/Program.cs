17c1c892fafbfc488f7adaafbef7ceeb2e921fdb
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-09 14:19:23 +0300

    добавил строки и колонки

--- Module_4/Exercise_4.3.10/Program.cs
+++ Module_4/Exercise_4.3.10/Program.cs
@@ -6,6 +6,8 @@
         {
             int[,] array = { { 1,2,3},{5,6,7 } };
             Console.WriteLine(array.Length);
+            Console.WriteLine($"Кол-во строк: { array.GetUpperBound(0)+1}");
+            Console.WriteLine($"Кол-во колонок: {array.GetUpperBound(1) + 1}");
         }
     }
 }
