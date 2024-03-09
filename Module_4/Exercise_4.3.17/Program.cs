c58a4f626161274c930975eda878933d8ca58e49
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-09 16:55:47 +0300

    Добавил Exercise_4.3.17

---
+++ Module_4/Exercise_4.3.17/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Exercise_4._3._17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,]array = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
+            var temp = 0;
+
+            Console.WriteLine("Текущий массив:");
+            for (int i=0;i<array.GetLength(0);i++)
+            {
+                for (int j=0;j<array.GetLength(1);j++)
+                {
+                    Console.Write($"{array[i,j]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    for (int k = j + 1; k < array.GetLength(1); k++)
+                    {
+                        if (array[i, j] > array[i, k])
+                        {
+                            temp = array[i, j];
+                            array[i, j] = array[i, k];
+                            array[i, k] = temp;
+                        }
+                        
+                    }
+                    Console.Write($"{array[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
