9c02e5454baf19e8f9f4be501eb38e5e94a64f38
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-09 15:45:12 +0300

    Добавил Exercise_4.3.13

---
+++ Module_4/Exercise_4.3.13/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercise_4._3._13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
+            var Summ = 0;
+
+            Console.WriteLine("Текущий массив:");
+            foreach(var item in arr)
+            {
+                Console.Write($"{item} ");
+                Summ += item;
+            }
+
+            Console.WriteLine($"\nСумма всех чисел = {Summ}");
+            Console.ReadKey();
+        }
+    }
+}
