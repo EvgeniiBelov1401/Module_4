454fc0a7c088c6f79a3a7a571883f4f999ed0b1d
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-09 14:29:12 +0300

    добавил вывод массива

--- Module_4/Exercise_4.3.10/Program.cs
+++ Module_4/Exercise_4.3.10/Program.cs
@@ -7,7 +7,17 @@
             int[,] array = { { 1,2,3},{5,6,7 } };
             Console.WriteLine(array.Length);
             Console.WriteLine($"Кол-во строк: { array.GetUpperBound(0)+1}");
-            Console.WriteLine($"Кол-во колонок: {array.GetUpperBound(1) + 1}");
+            Console.WriteLine($"Кол-во колонок: {array.GetUpperBound(1) + 1}\n");
+
+            for (int i=0;i<array.GetUpperBound(0)+1;i++)
+            {
+                for (int k=0;k<array.GetUpperBound(1)+1;k++)
+                {
+                    Console.Write($"{array[i, k]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
         }
     }
 }
