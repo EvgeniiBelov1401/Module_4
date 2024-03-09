91ea21fc770b22c88cf6acf41226746df8ddcc6b
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-09 17:49:43 +0300

    Добавил Exercise_4.4.5

---
+++ Module_4/Exercise_4.4.5/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercise_4._4._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            (string Name, string Type, double Age, int NameCount) Pet;
+
+            Console.Write("Тип животного: ");
+            Pet.Type = Console.ReadLine();
+            Console.Write("Имя животного: ");
+            Pet.Name = Console.ReadLine();
+            Console.Write("Возраст животного: ");
+            Pet.Age = double.Parse(Console.ReadLine());
+            Pet.NameCount = Pet.Name.Length;
+
+            Console.WriteLine("Показать всю информацию ");
+            Console.ReadKey();
+
+            Console.WriteLine($"{Pet.Type} по имени {Pet.Name}({Pet.NameCount}), возраст: {Pet.Age}");
+            Console.ReadKey();
+        }
+    }
+}
