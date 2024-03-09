e68d49e30b5e2935ee8e3e9c306e030f529a667f
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-09 17:36:55 +0300

    Добавил Exercise_4.4.4

---
+++ Module_4/Exercise_4.4.4/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Exercise_4._4._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            (string Name, string Type, double Age) Pet;
+            //var Pet=(Name:"Тимофей",Type:"Кот",Age:14);
+        }
+    }
+}
