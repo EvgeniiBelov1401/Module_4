493a6b3a5ab296073f055ca23ff755b2160410f0
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-09 13:46:01 +0300

    Добавил Exercise_4.3.5

---
+++ Module_4/Exercise_4.3.5/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercise_4._3._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите свое имя: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Ваше имя по буквам: ");
+            foreach (var ch in name)
+            {
+                Console.Write($"{ch} ");
+                char lastLetter = ch;
+            }
+            Console.WriteLine($"\nПоследняя буква: {name[name.Length-1]}") ;
+            Console.ReadKey();
+        }
+    }
+}
