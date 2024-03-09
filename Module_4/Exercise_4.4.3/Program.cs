1ae4d907892281d20c7aae9cd15f378d005d7295
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-09 17:26:30 +0300

    Добавил Exercise_4.4.3

---
+++ Module_4/Exercise_4.4.3/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercise_4._4._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var anketa=("Евгений",34);
+            Console.WriteLine($"Ваше имя: {anketa.Item1}\nВаш возраст: {anketa.Item2}");
+            Console.ReadKey();
+            Console.WriteLine();
+
+            Console.Write("Введите ваше имя: ");
+            anketa.Item1 = Console.ReadLine();
+            Console.Write("Введите ваш возраст: ");
+            anketa.Item2=int.Parse(Console.ReadLine());
+            Console.WriteLine($"\nВаше имя: {anketa.Item1}\nВаш возраст: {anketa.Item2}");
+            Console.ReadKey();
+        }
+    }
+}
