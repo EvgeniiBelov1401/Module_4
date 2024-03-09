0bd74c67147a4f4d6d2b760c456d2d61906ff375
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-09 16:34:00 +0300

    Добавил Exercise_4.3.16

---
+++ Module_4/Exercise_4.3.16/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Exercise_4._3._16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
+            var CounterPositiveNum = 0;
+
+
+            Console.WriteLine("Текущий массив:");
+            for (int i=0;i<array.GetLength(0);i++)
+            {
+                for (int j=0;j<array.GetLength(1);j++)
+                {
+                    Console.Write($"{array[i,j]} ");
+                    if (array[i, j] > 0)
+                    {
+                        CounterPositiveNum++;
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine($"Кол-во положительных чисел = {CounterPositiveNum}");
+            Console.ReadKey();
+        }
+    }
+}
