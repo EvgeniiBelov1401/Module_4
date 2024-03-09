bc1f38b7393a84a60b14b5b6360db50edfbb1a8a
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-09 15:07:01 +0300

    Добавил Exercise_4.3.12

---
+++ Module_4/Exercise_4.3.12/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Exercise_4._3._12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr=new int[] { 5,6,9,1,2,3,4 };
+            int Value;
+
+            Console.WriteLine("Текущий массив:");
+            for (int i=0; i<arr.Length;i++)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+            Console.ReadKey();
+            
+            for (int i=0;i<arr.Length;i++)
+            {
+                for (int k=i+1;k<arr.Length;k++)
+                {
+                    if (arr[i] > arr[k])
+                    {
+                        Value = arr[i];
+                        arr[i] = arr[k];
+                        arr[k] = Value;
+                    }
+                }
+            }
+            Console.WriteLine("Отсортированный массив: ");
+            foreach (var item in arr)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
